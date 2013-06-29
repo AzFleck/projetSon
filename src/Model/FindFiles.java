c518068c04c092f8d8602984a0ac4917326b1a18
Ouzeau Fabien <ouzeau.f@gmail.com>
2013-06-29 13:33:06 +0200

    modif

--- src/Model/FindFiles.java
+++ src/Model/FindFiles.java
@@ -115,11 +115,7 @@ public class FindFiles extends Observable implements Runnable {
 		if (file.endsWith(".avi") || file.endsWith(".mp4") || file.endsWith(".mp3") || file.endsWith(".mkv")) {
 			fileSelect.setIdFile(lastId);
 			fileSelect.setTitle(file.substring(0, file.length() - 4));
-<<<<<<< HEAD
-			fileSelect.setPath(pathOfSubDirectory + "\\" + file);
-=======
 			fileSelect.setPath(pathOfSubDirectory + "\\" +file);
->>>>>>> c70b3cd6cdd0448b3628da7de5a116f106286fd1
 			fileSelect.setDate("2000-01-01");
 			fileSelect.setFind(true);
 			if (!checkExistentFile(fileSelect.getPath())) {
